7c7069efc5e54470e48732b7864d80f6e3337bc2
jamilebastos <jamilebastos61@gmail.com>
2025-05-24 15:37:49 -0300

    Add files via upload

---
+++ prova condicional/prova.py
@@ -0,0 +1,10 @@
+numero = int(input ("Digite um numero:  "))
+
+if numero > 0 :
+    print (f"O numero {numero} é positivo")
+
+elif numero < 0:
+    print (f"O numero {numero} é negativo")
+
+else:
+    print(f"O numero{numero} é 0")
